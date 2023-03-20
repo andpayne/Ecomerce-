611c25d4f709608be760ba1af5c2ffb35289582c
Andres <andpace89@gmail.com>
2023-03-20 18:06:26 -0300

    mi primer commit

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,16 @@
+const NavBar = () => {
+    return (
+        <nav>
+            <h1>Ecomerce</h1>
+            <ul>
+                <li><a>href="index.html"</a>Inicio</li>
+                <li><a>href="telefonos.html"</a>Telefonos</li>
+                <li><a>href="computadoras.html"</a>Computadoras</li>
+                <li><a>href="accesorios.html"</a>Accesorios</li>
+                <li><a>href="televisores.html"</a>Televisores</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default NavBar
